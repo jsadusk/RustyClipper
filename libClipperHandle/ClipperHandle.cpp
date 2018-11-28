2845c8dd17cc363a7afc34c321583a793b17b966
Joseph Sadusk <jsadusk@gmail.com>
2018-11-28 02:34:31 -0800

    Wrapped a ton of new stuff, the clipper object, clipperoffset, and added an implementation for the list of paths

--- libClipperHandle/ClipperHandle.cpp
+++ libClipperHandle/ClipperHandle.cpp
@@ -15,6 +15,16 @@ int path_new(void **obj, ClipperLib::IntPoint **data) {
     return 0;
 }
 
+int path_data(const void *obj, ClipperLib::IntPoint **data) {
+    try {
+        ClipperLib::Path &path = *(ClipperLib::Path*)obj;
+        *data = path.data();
+    } catch (...) {
+        return 1;
+    }
+
+    return 0;
+}
 
 int path_new_sized(size_t size, void **obj, ClipperLib::IntPoint **data) {
     try {
@@ -46,11 +56,146 @@ void path_delete(void *obj) {
     delete path;
 }
 
+int paths_new(void **obj, void **data) {
+    try {
+        ClipperLib::Paths *newPaths = new ClipperLib::Paths();
+        *obj = (void*)newPaths;
+        *data = newPaths->data();
+    } catch (...) {
+        return 1;
+    }
+
+    return 0;
+}
+
+
+int paths_new_sized(size_t size, void **obj, void **data) {
+    try {
+        ClipperLib::Paths *newPaths = new ClipperLib::Paths(size);
+        *obj = (void*)newPaths;
+        *data = newPaths->data();
+    } catch (...) {
+        return 1;
+    }
+
+    return 0;
+}
+
+int paths_data(const void *obj, void **data) {
+    try {
+        ClipperLib::Paths &paths = *(ClipperLib::Paths*)obj;
+        *data = paths.data();
+    } catch (...) {
+        return 1;
+    }
+}
+
+int paths_push_back_move(void *obj, void *elem, void **data) {
+    try {
+        ClipperLib::Paths &paths = *(ClipperLib::Paths*)obj;
+        ClipperLib::Path &path = *(ClipperLib::Path*)elem;
+        paths.push_back(std::move(path));
+        *data = paths.data();
+    } catch (...) {
+        return 1;
+    }
+
+    return 0;
+}
+
+void paths_delete(void *obj) {
+    ClipperLib::Paths *paths = (ClipperLib::Paths*)obj;
+    delete paths;
+}
+
 int test_elem(void *obj, size_t index, long test_x, long test_y) {
     ClipperLib::Path &path = *(ClipperLib::Path*)obj;
     ClipperLib::IntPoint &elem = path[index];
 
     return elem.X == test_x && elem.Y == test_y;
+}
+
+int clipper_new(void **obj, int initOptions) {
+    try {
+        ClipperLib::Clipper *newclipper = new ClipperLib::Clipper(initOptions);
+        *obj = (void*)newclipper;
+    } catch (...) {
+        return 1;
+    }
+    
+    return 0;
+}
+
+int clipper_add_path(void *clipper_obj, const void *path_obj,
+                     const ClipperLib::PolyType poly_type,
+                     int closed) {
+    try {
+        ClipperLib::Clipper *clipper = (ClipperLib::Clipper*)clipper_obj;
+        const ClipperLib::Path *path = (ClipperLib::Path*)path_obj;
+        clipper->AddPath(*path, poly_type, closed);
+    } catch (...) {
+        return 1;
+    }
+
+    return 0;
+}
+
+int clipper_offset_add_path(void *clipper_offset_obj, const void *path_obj,
+                            const ClipperLib::JoinType join_type,
+                            const ClipperLib::EndType end_type) {
+    try {
+        ClipperLib::ClipperOffset *clipper_offset = (ClipperLib::ClipperOffset*)clipper_offset_obj;
+        const ClipperLib::Path *path = (ClipperLib::Path*)path_obj;
+        clipper_offset->AddPath(*path, join_type, end_type);
+    } catch (...) {
+        return 1;
+    }
+
+    return 0;
+}
+
+int clipper_execute_open_closed(
+      void *clipper_obj, void **solution_open_obj, void **solution_closed_obj,
+      const ClipperLib::ClipType clip_type,
+      const ClipperLib::PolyFillType subj_fill_type,
+      const ClipperLib::PolyFillType clip_fill_type) {
+    try {
+        ClipperLib::Clipper *clipper = (ClipperLib::Clipper*)clipper_obj;
+        ClipperLib::PolyTree solution;
+        clipper->Execute(clip_type, solution, subj_fill_type, clip_fill_type);
+        ClipperLib::Paths *solution_open = new ClipperLib::Paths();
+        ClipperLib::OpenPathsFromPolyTree(solution, *solution_open);
+        ClipperLib::Paths *solution_closed = new ClipperLib::Paths();
+        ClipperLib::ClosedPathsFromPolyTree(solution, *solution_closed);
+        *solution_open_obj = (void*)solution_open;
+        *solution_closed_obj = (void*)solution_closed;
+    } catch (...) {
+        return 1;
+    }
+
+    return 0;
+}
+
+int clipper_delete(void *obj) {
+    ClipperLib::Clipper *clipper = (ClipperLib::Clipper*)obj;
+    delete clipper;
+}
+
+int clipper_offset_new(void **obj) {
+    try {
+        ClipperLib::ClipperOffset *newClipperOffset =
+            new ClipperLib::ClipperOffset();
+        *obj = (void*)newClipperOffset;
+    } catch (...) {
+        return 1;
+    }
+
+    return 0;
+}
+
+int clipper_offset_delete(void **obj) {
+    ClipperLib::ClipperOffset *clipperOffset = (ClipperLib::ClipperOffset*)obj;
+    delete clipperOffset;
 }
 
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,8 @@ use std::ops::{Index, IndexMut};
 
 #[derive(Fail, Debug)]
 pub enum ClipperError {
-    #[fail(display = "Unexpected C++ exception")]
-    CppException
+    #[fail(display = "Unexpected C++ exception in call {}", call)]
+    CppException { call : &'static str }
 }
 
 type ClipperResult<T> = Result<T, ClipperError>;
@@ -20,20 +20,81 @@ pub struct CppIntPoint {
     pub y : i64
 }
 
-enum CppPathObj {}
+pub enum CppPathObj {}
+pub enum CppPathsObj {}
+enum CppClipperObj {}
+enum CppClipperOffsetObj {}
+
+pub enum CppPolyType {
+    PtSubject = 0,
+    PtClip = 1
+}
+
+pub enum CppJoinType {
+    JtSquare = 0,
+    JtRound = 1,
+    JtMitre = 2
+}
+
+pub enum CppEndType {
+    EtClosedPolygon = 0,
+    EtClosedLine = 1,
+    EtOpenSquare = 2,
+    EtOpenRound = 3,
+    EtOpenButt = 4
+}
 
 #[link(name = "ClipperHandle")]
 extern {
     fn path_new(obj: *mut *mut CppPathObj, data: *mut *mut CppIntPoint) -> i32;
-    fn path_new_sized(size : size_t, obj: *mut *mut CppPathObj, data: *mut *mut CppIntPoint) -> i32;
+    fn path_new_sized(size : size_t, obj: *mut *mut CppPathObj,
+                      data: *mut *mut CppIntPoint) -> i32;
+    fn path_data(obj: *const CppPathObj, data: *mut *mut CppIntPoint) ->i32;
     fn path_push_back(obj: *mut CppPathObj, elem: *const CppIntPoint,
                       data: *mut *mut CppIntPoint) ->i32;
     fn path_delete(obj: *mut CppPathObj);
+
+    fn paths_new(obj: *mut *mut CppPathsObj, data: *mut *mut CppPathObj) -> i32;
+    fn paths_new_sized(size : size_t, obj: *mut *mut CppPathsObj,
+                      data: *mut *mut CppPathObj) -> i32;
+    fn paths_data(obj: *const CppPathsObj, data: *mut *mut CppPathObj) ->i32;
+    fn paths_push_back_move(obj: *mut CppPathsObj, elem: *mut CppPathObj,
+                            data: *mut *mut CppPathObj) ->i32;
+    fn paths_delete(obj: *mut CppPathsObj);
+
+    fn clipper_new(obj: *mut *mut CppClipperObj, initOptions : i32) ->i32;
+    fn clipper_add_path(clipper_obj: *mut CppClipperObj,
+                        path_obj : *const CppPathObj,
+                        poly_type : i32,
+                        closed : i32) ->i32;
+    fn clipper_delete(obj: *mut CppClipperObj);
+
+    fn clipper_offset_new(obj: *mut *mut CppClipperOffsetObj) ->i32;
+    fn clipper_offset_add_path(clipper_obj: *mut CppClipperOffsetObj,
+                               path_obj: *const CppPathObj,
+                               join_type: i32,
+                               end_type: i32) ->i32;
+    fn clipper_offset_delete(obj: *mut CppClipperOffsetObj);
 }
 
 pub struct CppPath {
     obj: *mut CppPathObj,
-    data: *mut CppIntPoint
+    data: *mut CppIntPoint,
+    owned: bool
+}
+
+pub struct CppPaths {
+    obj: *mut CppPathsObj,
+    data: *mut CppPathObj,
+    owned: bool
+}
+
+pub struct Clipper {
+    obj : *mut CppClipperObj
+}
+
+pub struct ClipperOffset {
+    obj : *mut CppClipperOffsetObj
 }
 
 impl CppPath {
@@ -44,9 +105,9 @@ impl CppPath {
             let code = path_new_sized(size, &mut obj, &mut data);
 
             if code == 0 {
-                Ok(CppPath { obj : obj, data : data })
+                Ok(CppPath { obj : obj, data : data, owned : true })
             } else {
-                Err(ClipperError::CppException)
+                Err(ClipperError::CppException { call : "path_new_sized" })
             }
         }
     }
@@ -58,23 +119,45 @@ impl CppPath {
             let code = path_new(&mut obj, &mut data);
 
             if code == 0 {
-                Ok(CppPath { obj : obj, data : data })
+                Ok(CppPath { obj : obj, data : data, owned : true })
             } else {
-                Err(ClipperError::CppException)
+                Err(ClipperError::CppException { call : "path_new" })
             }
         }
     }
 
-    pub fn push(&mut self, elem : &CppIntPoint) -> ClipperResult<()> {
+    pub(crate) fn from(obj : *mut CppPathObj) -> ClipperResult<CppPath> {
         unsafe {
-            let code = path_push_back(self.obj, elem, &mut self.data);
+            let mut data: *mut CppIntPoint = ptr::null_mut();
+            let code = path_data(obj, &mut data);
+
+            if code == 0 {
+                Ok(CppPath { obj : obj, data : data, owned : false })
+            } else {
+                Err(ClipperError::CppException { call : "path_data" })
+            }
+        }
+    }
+
+    pub fn push(&mut self, elem : CppIntPoint) -> ClipperResult<()> {
+        unsafe {
+            let code = path_push_back(self.obj, &elem, &mut self.data);
 
             if code == 0 {
                 Ok(())
             } else {
-                Err(ClipperError::CppException)
+                Err(ClipperError::CppException { call : "path_push_back" })
             }
         }
+    }
+
+    pub(crate) fn to_handle(mut self) -> *mut CppPathObj {
+        self.owned = false;
+        self.obj
+    }
+
+    pub(crate) fn handle(&self) -> *const CppPathObj {
+        self.obj as *const CppPathObj
     }
 }
 
@@ -98,8 +181,163 @@ impl IndexMut<usize> for CppPath {
 
 impl Drop for CppPath {
     fn drop(& mut self) {
+        if self.owned {
+            unsafe {
+                path_delete(self.obj);
+            }
+        }
+    }
+}
+
+impl CppPaths {
+    pub fn new_sized(size : usize) -> ClipperResult<CppPaths> {
         unsafe {
-            path_delete(self.obj);
+            let mut obj: *mut CppPathsObj = ptr::null_mut();
+            let mut data: *mut CppPathObj = ptr::null_mut();
+            let code = paths_new_sized(size, &mut obj, &mut data);
+
+            if code == 0 {
+                Ok(CppPaths { obj : obj, data : data, owned : true })
+            } else {
+                Err(ClipperError::CppException { call : "paths_new_sized" })
+            }
+        }
+    }
+
+    pub fn new() -> ClipperResult<CppPaths> {
+        unsafe {
+            let mut obj: *mut CppPathsObj = ptr::null_mut();
+            let mut data: *mut CppPathObj = ptr::null_mut();
+            let code = paths_new(&mut obj, &mut data);
+
+            if code == 0 {
+                Ok(CppPaths { obj : obj, data : data, owned : true })
+            } else {
+                Err(ClipperError::CppException { call : "paths_new" })
+            }
+        }
+    }
+
+    pub(crate) fn from(obj : *mut CppPathsObj) -> ClipperResult<CppPaths> {
+        unsafe {
+            let mut data: *mut CppPathObj = ptr::null_mut();
+            let code = paths_data(obj, &mut data);
+
+            if code == 0 {
+                Ok(CppPaths { obj : obj, data : data, owned : false })
+            } else {
+                Err(ClipperError::CppException { call : "paths_data" })
+            }
+        }
+    }
+
+    pub fn push(&mut self, elem : CppPath) -> ClipperResult<()> {
+        unsafe {
+            let code = paths_push_back_move(self.obj, elem.to_handle(),
+                                            &mut self.data);
+
+            if code == 0 {
+                Ok(())
+            } else {
+                Err(ClipperError::CppException { call : "paths_push_back_move" })
+            }
+        }
+    }
+
+    pub(crate) fn to_handle(mut self) -> *mut CppPathsObj {
+        self.owned = false;
+        self.obj
+    }
+
+    pub(crate) fn handle(self) -> *const CppPathsObj {
+        self.obj as *const CppPathsObj
+    }
+
+    pub fn at(&mut self, idx : usize) -> ClipperResult<CppPath> {
+        CppPath::from(unsafe { self.data.offset(idx as isize) })
+    }
+}
+
+impl Drop for CppPaths {
+    fn drop(& mut self) {
+        if self.owned {
+            unsafe {
+                paths_delete(self.obj);
+            }
+        }
+    }
+}
+
+impl Clipper {
+    pub fn new() -> ClipperResult<Clipper> {
+        unsafe {
+            let mut obj: *mut CppClipperObj = ptr::null_mut();
+            let code = clipper_new(&mut obj, 0);
+            if code == 0 {
+                Ok(Clipper { obj : obj})
+            } else {
+                Err(ClipperError::CppException { call : "clipper_new" })
+            }
+        }
+    }
+
+    pub fn add_path(&mut self,
+                path : &CppPath, poly_type : CppPolyType, closed : bool)
+                -> ClipperResult<()> {
+        unsafe {
+            let code = clipper_add_path(self.obj, path.handle(),
+                                        poly_type as i32,
+                                        match closed { true => 1, false => 0});
+            if code == 0 {
+                Ok(())
+            } else {
+                Err(ClipperError::CppException { call : "clipper_add_path" })
+            }
+        }
+    }
+}
+
+impl Drop for Clipper {
+    fn drop(&mut self) {
+        unsafe {
+            clipper_delete(self.obj);
+        }
+    }
+}
+
+impl ClipperOffset {
+    pub fn new() -> ClipperResult<ClipperOffset> {
+        unsafe {
+            let mut obj: *mut CppClipperOffsetObj = ptr::null_mut();
+            let code = clipper_offset_new(&mut obj);
+            if code == 0 {
+                Ok(ClipperOffset { obj : obj })
+            } else {
+                Err(ClipperError::CppException { call : "clipper_offset_new" })
+            }
+        }
+    }
+
+    pub fn add_path(&mut self,
+                path : &CppPath, join_type : CppJoinType, end_type: CppEndType)
+                -> ClipperResult<()> {
+        unsafe {
+            let code = clipper_offset_add_path(self.obj, path.handle(),
+                                               join_type as i32,
+                                               end_type as i32);
+            if code == 0 {
+                Ok(())
+            } else {
+                Err(ClipperError::CppException { call : "clipper_offset_add_path" })
+            }
+        }
+    }
+}
+
+impl Drop for ClipperOffset {
+    fn drop(&mut self) {
+        unsafe {
+            clipper_offset_delete(self.obj);
         }
     }
 }
@@ -157,11 +395,11 @@ mod tests {
     fn test_cpp_path_push() {
         let mut path = CppPath::new().unwrap();
 
-        path.push(&CppIntPoint { x: 1, y: 2}).unwrap();
-        path.push(&CppIntPoint { x: 2, y: 3}).unwrap();
-        path.push(&CppIntPoint { x: 3, y: 4}).unwrap();
-        path.push(&CppIntPoint { x: 4, y: 5}).unwrap();
-        path.push(&CppIntPoint { x: 5, y: 6}).unwrap();
+        path.push(CppIntPoint { x: 1, y: 2}).unwrap();
+        path.push(CppIntPoint { x: 2, y: 3}).unwrap();
+        path.push(CppIntPoint { x: 3, y: 4}).unwrap();
+        path.push(CppIntPoint { x: 4, y: 5}).unwrap();
+        path.push(CppIntPoint { x: 5, y: 6}).unwrap();
 
         assert_eq!(path[0].x, 1);
         assert_eq!(path[1].x, 2);
@@ -176,5 +414,85 @@ mod tests {
             assert!(test_elem(path.obj, 3, 4, 5) != 0);
             assert!(test_elem(path.obj, 4, 5, 6) != 0);
         }
+    }
+
+    #[test]
+    fn test_clipper_new_drop() {
+        let _clipper = Clipper::new().unwrap();
+    }        
+
+    #[test]
+    fn test_clipper_offset_new_drop() {
+        let _clipper_offset = ClipperOffset::new().unwrap();
+    }
+
+    #[test]
+    fn test_clipper_add_path() {
+        let mut clipper = Clipper::new().unwrap();
+        let mut path = CppPath::new().unwrap();
+        path.push(CppIntPoint { x: 0, y: 0}).unwrap();
+        path.push(CppIntPoint { x: 1, y: 1}).unwrap();
+        path.push(CppIntPoint { x: 1, y: 0}).unwrap();
+        path.push(CppIntPoint { x: 0, y: 0}).unwrap();
+        clipper.add_path(&path, CppPolyType::PtClip, true).unwrap();
+        let mut path = CppPath::new().unwrap();
+        path.push(CppIntPoint { x: 0, y: 0}).unwrap();
+        path.push(CppIntPoint { x: -1, y: -1}).unwrap();
+        path.push(CppIntPoint { x: -1, y: 0}).unwrap();
+        clipper.add_path(&path, CppPolyType::PtSubject, false).unwrap();
+    }
+
+    #[test]
+    fn test_clipper_offset_add_path() {
+        let mut clipper_offset = ClipperOffset::new().unwrap();
+        let mut path = CppPath::new().unwrap();
+        path.push(CppIntPoint { x: 0, y: 0}).unwrap();
+        path.push(CppIntPoint { x: 1, y: 1}).unwrap();
+        path.push(CppIntPoint { x: 1, y: 0}).unwrap();
+        path.push(CppIntPoint { x: 0, y: 0}).unwrap();
+        clipper_offset.add_path(&path, CppJoinType::JtRound,
+                         CppEndType::EtClosedPolygon).unwrap();
+        let mut path = CppPath::new().unwrap();
+        path.push(CppIntPoint { x: 0, y: 0}).unwrap();
+        path.push(CppIntPoint { x: -1, y: -1}).unwrap();
+        path.push(CppIntPoint { x: -1, y: 0}).unwrap();
+        clipper_offset.add_path(&path, CppJoinType::JtMitre,
+                                CppEndType::EtOpenRound).unwrap();
+    }
+
+    #[test]
+    fn test_path_from() {
+        let mut path1 = CppPath::new().unwrap();
+        let mut path2 = CppPath::from(path1.to_handle()).unwrap();
+    }
+    
+    #[test]
+    fn test_paths_new_drop() {
+        let _paths = CppPaths::new().unwrap();
+    }
+
+    #[test]
+    fn test_paths_new_sized() {
+        let mut paths = CppPaths::new_sized(3).unwrap();
+        let mut path0 = paths.at(0).unwrap();
+        path0.push(CppIntPoint { x: 1, y: 2 }).unwrap();
+        let mut path1 = paths.at(1).unwrap();
+        path1.push(CppIntPoint { x: 1, y: 2 }).unwrap();
+        let mut path2 = paths.at(2).unwrap();
+        path2.push(CppIntPoint { x: 1, y: 2 }).unwrap();
+    }
+
+    #[test]
+    fn test_paths_push() {
+        let mut path0 = CppPath::new().unwrap();
+        path0.push(CppIntPoint { x: 1, y: 2}).unwrap();
+
+        let mut paths = CppPaths::new().unwrap();
+        paths.push(path0);
+
+        let mut path1 = paths.at(0).unwrap();
+        let mut point = &path1[0];
+        assert!(point.x == 1);
+        assert!(point.y == 2);
     }
 }
