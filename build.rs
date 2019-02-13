5e780bd4203b0905d22d5000ada25655a69fac2e
Patrick O'Day <pcoday@gmail.com>
2019-02-13 03:24:33 -0800

    Adding a conditional compilation for macos as xcode 10+ has deprecated libstdc++ for libc++

--- build.rs
+++ build.rs
@@ -5,6 +5,10 @@ fn main() {
     let dst = Config::new("libClipperHandle").build();
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=static=ClipperHandle");
-    println!("cargo:rustc-link-lib=dylib=stdc++");
+    if cfg!(target_os="macos"){
+        println!("cargo:rustc-link-lib=dylib=c++");
+    } else {
+        println!("cargo:rustc-link-lib=dylib=stdc++");
+    }
     println!("cargo:rustc-link-lib=dylib=polyclipping");
 }
